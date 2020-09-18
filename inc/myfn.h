f548f2cb586b15277fb35b0ed729518e695fdb99
99002436 <71481120+O-Deepak@users.noreply.github.com>
2020-09-18 21:22:09 +0530

    food ordering

--- inc/myfn.h
+++ inc/myfn.h
@@ -1,11 +1,17 @@
 #ifndef __MYFN_H__
 #define __MYFN_H__
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<windows.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdio_ext.h>
+void insertend(head *t, float data, char foodname[25], int quantity, float price);
+void ccolor(int clr);
+void foodlist(head *t);
+void foodlist1(head *t);
+float getbill(head *t);
+void order(head *t, head *l, float fc, int qty);
+void cls();
 
 
-void printop(int a);
-
-#endif  
+#endif
--- src/myfn.c
+++ src/myfn.c
@@ -1,7 +1,103 @@
 #include<myfn.h>
 
-
-void printop(int a)
+void insertend(head *t, float data, char foodname[25], int quantity, float price)
 {
-	printf("hello %d",a);
+    node *p, *q;
+    p=(node *)malloc(sizeof(node));
+    p->data=data;
+    p->quantity=quantity;
+    p->price=price;
+    strcpy(p->foodname, foodname);
+    p->next=NULL;
+    if(t->start==NULL)
+    {
+        t->start=p;
+    }
+    else
+    {
+        q=t->start;
+        while(q->next!=NULL)
+        {
+            q=q->next;
+        }
+        q->next=p;
+    }
 }
+void ccolor(int clr)
+{
+
+	HANDLE  hConsole;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, clr);
+
+}
+void foodlist(head *t)
+{
+
+	printf("\n\t"); ccolor(240);
+	printf("_______________________________________________________");ccolor(26);
+	printf("\n\t"); ccolor(240);
+	printf("  Food Code          Name                Price         ");ccolor(26);
+	printf("\n\t"); ccolor(240);
+	printf("-------------------------------------------------------");ccolor(26);
+
+	node *p;
+    p=t->start;
+    while(p!= NULL)
+    {
+        printf("\n\t");
+        printf("  %0.1f   |  %-25s     |    %0.2f   ",p->data,p->foodname,p->price);
+		ccolor(26);
+		printf("\n\t");
+        printf("-------------------------------------------------------");
+        p=p->next;
+    }
+}
+void foodlist1(head *t)
+{
+	printf("\n\t"); ccolor(240);
+	printf("___________________________________________________________________");ccolor(26);
+	printf("\n\t"); ccolor(240);
+	printf("  Food Code       Name                       Quantity      Price   ");ccolor(26);
+	printf("\n\t"); ccolor(240);
+	printf("-------------------------------------------------------------------");ccolor(26);
+
+	node *p;
+    p=t->start;
+    while(p!= NULL)
+    {
+		printf("\n\t");
+        printf("  %0.1f   |  %-25s      |    %2d      |  %0.2f   ",p->data,p->foodname, p->quantity, p->price);
+		printf("\n\t");
+		printf("------------------------------------------------------------------");
+        p=p->next;
+    }
+}
+float getbill(head *t)
+{
+    node *p;
+    float b=0;
+    p=t->start;
+    while(p!=NULL)
+    {
+        b=b+p->price;
+        p=p->next;
+    }
+    return b;
+}
+void order(head *t, head *l, float fc, int qty)
+{
+    node *p;
+    p=t->start;
+    while(p->data!=fc)
+    {
+        p=p->next;
+    }
+    insertend(l, fc, p->foodname, qty, (p->price*qty));
+    return;
+}
+void cls()
+{
+    system("cls");
+}
+
