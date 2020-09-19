bcdcd5285d3343ea0a71f4447681b5ccc098494d
99002436 <71481120+O-Deepak@users.noreply.github.com>
2020-09-19 13:34:30 +0530

    Update myfn.c

--- src/myfn.c
+++ src/myfn.c
@@ -29,11 +29,11 @@ void foodlist(head *t)
 {
 
 	printf("\n\t");
-	printf("_______________________________________________________");
+	
 	printf("\n\t"); 
 	printf("  Food Code          Name                Price         ");
 	printf("\n\t"); 
-	printf("-------------------------------------------------------");
+	
 
 	node *p;
     p=t->start;
@@ -43,18 +43,18 @@ void foodlist(head *t)
         printf("  %0.1f   |  %-25s     |    %0.2f   ",p->data,p->foodname,p->price);
 		
 		printf("\n\t");
-        printf("-------------------------------------------------------");
+       
         p=p->next;
     }
 }
 void foodlist1(head *t)
 {
 	printf("\n\t");
-	printf("___________________________________________________________________");
+	
 	printf("\n\t"); 
 	printf("  Food Code       Name                       Quantity      Price   ");
 	printf("\n\t"); 
-	printf("-------------------------------------------------------------------");
+	
 
 	node *p;
     p=t->start;
@@ -63,7 +63,7 @@ void foodlist1(head *t)
 		printf("\n\t");
         printf("  %0.1f   |  %-25s      |    %2d      |  %0.2f   ",p->data,p->foodname, p->quantity, p->price);
 		printf("\n\t");
-		printf("------------------------------------------------------------------");
+		
         p=p->next;
     }
 }
